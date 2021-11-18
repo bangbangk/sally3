b193208c79a326c07bb9a4c84699463275ce2f3f
GreenArt <GreenArt@303PC-13>
2021-11-18 12:30:14 +0900

    공지사항 수정

--- sally/src/main/java/org/zerock/controller/NoticeController.java
+++ sally/src/main/java/org/zerock/controller/NoticeController.java
@@ -1,36 +1,28 @@
 package org.zerock.controller;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.zerock.domain.NoticeCriteria;
+import org.zerock.domain.NoticePageDTO;
 import org.zerock.domain.NoticeVO;
-import org.zerock.domain.Page;
 import org.zerock.service.NoticeService;
+
+import lombok.extern.log4j.Log4j;
 	
 //@SuppressWarnings("unused")
+@Log4j
 @Controller
 @RequestMapping("/notice/*")
 public class NoticeController {
 	
 	@Inject
 	NoticeService service;
-	
-	// 게시물 목록
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void getList(Model model) throws Exception {
-		 
-		List<NoticeVO> list = null;
-		list = service.list();
-		
-		model.addAttribute("list", list);
-	}
 	
 	// 게시물 작성
 	@RequestMapping(value = "/write", method = RequestMethod.GET)
@@ -48,9 +40,10 @@ public class NoticeController {
 	
 	// 게시물 조회
 	@RequestMapping(value = "/view", method = RequestMethod.GET)
-	public void getView(@RequestParam("bno") int bno, Model model) throws Exception{
+	public void getView(@RequestParam("bno") int bno, Model model, NoticeCriteria cri) throws Exception{
 		NoticeVO vo = service.view(bno);
 		model.addAttribute("view", vo);
+		model.addAttribute("cri", cri);
 	}
 	
 	// 게시물 수정
@@ -80,111 +73,19 @@ public class NoticeController {
 	}
 	
 	// 게시물 목록 + 페이징 추가
-	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
-	public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
-	 
-		Page page = new Page();
-		
-		page.setNum(num);
-		page.setCount(service.count());  
-
-		List<NoticeVO> list = null; 
-		list = service.listPage(page.getDisplayPost(), page.getPostNum());
-
-		model.addAttribute("list", list);   
-/*		model.addAttribute("pageNum", page.getPageNum());
-
-		model.addAttribute("startPageNum", page.getStartPageNum());
-		model.addAttribute("endPageNum", page.getEndPageNum());
-		 
-		model.addAttribute("prev", page.getPrev());
-		model.addAttribute("next", page.getNext());  */
-
-		model.addAttribute("page", page);
-		model.addAttribute("select", num);
-		
-	/*
-	// 게시물 총 갯수
-	int count = service.count();
-	  
-	// 한 페이지에 출력할 게시물 갯수
-	int postNum = 20;
-	 
-	// 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
-	int pageNum = (int)Math.ceil((double)count/postNum);
-	 
-	// 출력할 게시물
-	int displayPost = (num - 1) * postNum;
-	 
-	 
-	 
-	// 한번에 표시할 페이징 번호의 갯수
-	int pageNum_cnt = 20;
+	@GetMapping("/list")
+	public void getListPage(Model model, NoticeCriteria cri) {
+        
+        log.info("getListPage");
+        
+        model.addAttribute("list", service.listPage(cri));
+        
+        int total = service.getTotal(cri);
+        
+        NoticePageDTO pageMake = new NoticePageDTO(cri, total);
+        
+        model.addAttribute("pageMake", pageMake);
+    }
 	
-	// 표시되는 페이지 번호 중 마지막 번호
-	int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
-	 
-	// 표시되는 페이지 번호 중 첫번째 번호
-	int startPageNum = endPageNum - (pageNum_cnt - 1);
-	    
-	// 마지막 번호 재계산
-	int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
-	  
-	if(endPageNum > endPageNum_tmp) {
-		endPageNum = endPageNum_tmp;
-	}
-	 
-	boolean prev = startPageNum == 1 ? false : true;
-	boolean next = endPageNum * pageNum_cnt >= count ? false : true;
-	 
-	List<NoticeVO> list = null; 
-	list = service.listPage(displayPost, postNum);
-	model.addAttribute("list", list);   
-	model.addAttribute("pageNum", pageNum);
-	 
-	// 시작 및 끝 번호
-	model.addAttribute("startPageNum", startPageNum);
-	model.addAttribute("endPageNum", endPageNum);
-
-	// 이전 및 다음 
-	model.addAttribute("prev", prev);
-	model.addAttribute("next", next);
-	
-	// 현재 페이지
-	model.addAttribute("select", num);
-	*/
-	
-	}
-	
-	// 게시물 목록 + 페이징 추가 + 검색
-	@RequestMapping(value = "/listPageSearch", method = RequestMethod.GET)
-	public void getListPageSearch(Model model, @RequestParam("num") int num, 
-			@RequestParam(value = "searchType",required = false, defaultValue = "title") String searchType,
-			@RequestParam(value = "keyword",required = false, defaultValue = "") String keyword
-			) throws Exception {
-		 
-			Page page = new Page();
-			
-			page.setNum(num);
-			//page.setCount(service.count());  
-			page.setCount(service.searchCount(searchType, keyword));
-
-			// 검색 타입과 검색어
-			//page.setSearchTypeKeyword(searchType, keyword);
-			page.setSearchType(searchType);
-			page.setKeyword(keyword);
-			
-			List<NoticeVO> list = null; 
-			//list = service.listPage(page.getDisplayPost(), page.getPostNum());
-			list = service.listPageSearch(page.getDisplayPost(), page.getPostNum(), searchType, keyword);
-
-			model.addAttribute("list", list);   
-			model.addAttribute("page", page);
-			model.addAttribute("select", num);
-			
-			model.addAttribute("searchType", searchType);
-			model.addAttribute("keyword", keyword);
-		
-		}
 	
 }
--- sally/src/main/webapp/WEB-INF/views/notice/view.jsp
+++ sally/src/main/webapp/WEB-INF/views/notice/view.jsp
@@ -1,7 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
-<%@include file="../include/header.jsp" %>
+<%@include file="../include/include_header.jsp"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -42,7 +42,7 @@
                 </tbody>
             </table>
             <div class="list">
-                <a href="javascript:history.back();">목록</a>
+                <a id="list_btn" style="cursor: pointer">목록</a><!--  href="javascript:history.back();"-->
             </div>
         </div>
     </div>
@@ -51,6 +51,25 @@
 		<a href="/notice/modify?bno=${view.bno}">게시물 수정</a>
 		<a href="/notice/delete?bno=${view.bno}">게시물 삭제</a>
 	</div> -->
-	<%@include file="../include/footer.jsp" %>
+	
+	<form id="infoForm" action="/notice/modify" method="get">
+		<input type="hidden" id="bno" name="bno" value='<c:out value="${view.bno}"/>'>
+		<input type="hidden" name="pageNum" value='<c:out value="${cri.pageNum}"/>'>
+		<input type="hidden" name="amount" value='<c:out value="${cri.amount}"/>'>
+		<input type="hidden" name="keyword" value="${pageMake.cri.keyword}">
+        <input type="hidden" name="type" value="${pageMake.cri.type }">
+	</form>
+	
+    <%@include file="../include/include_footer.jsp"%>
+	
+	<script>
+		let form = $("#infoForm");
+		
+		$("#list_btn").on("click", function(e){
+			form.find("#bno").remove();
+			form.attr("action", "/notice/list");
+			form.submit();
+		})
+	</script>
 </body>
 </html>
